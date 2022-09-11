f9ab7bcd552236f1c4b5cef1b7a59ae254457088
JM_Medina <juamedrod@hotmail.es>
2022-09-11 12:30:59 +0200

    Alpha V1.0.0

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -10,6 +11,18 @@ router.post('/chatbot', async (req, res) => {
     const response = await global.nlp.process(req.body.input);
     res.json(response);
 });
+
+router.post('/get_bonos', async (req, res) => {
+    console.log(req.body);
+    res.json({ answer: `El telefono ${req.body.input} puede contratar un bono de ${Math.floor(Math.random() * 15)}GB  por solo 1250€ (mas I.V.A.)` });
+});
+
+router.post('/get_saldo', async (req, res) => {
+    console.log(req.body);
+    res.json({ answer: `El telefono ${req.body.input} tiene un saldo activo de ${Math.floor(Math.random() * 101)}€` });
+});
+
+
 
 
 module.exports = router;
--- statics/index.ejs.js
+++ statics/index.ejs.js
@@ -3,6 +3,7 @@ const inputChat = document.getElementById('inputChat');
 const preRef = document.getElementById('result');
 
 const chatArr = [{ user: 'bot', message: 'Bienvenido a JuanmiPhone, ¿en qué puedo ayudarte?' }];
+let intent = 'app.hi';
 
 for (const chat of chatArr) {
     chatRef.innerHTML += `<p class=${chat.user}>${chat.message}</p>`;
@@ -14,17 +15,45 @@ async function addChatMessage(event) {
     console.log(event.target.value);
     chatArr.push({ user: 'user', message: event.target.value });
     const body = JSON.stringify({ input: event.target.value });
-    const response = await fetch('/chatbot',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: body
-        });
+    let response;
+    switch (intent) {
+        case "app.bonos":
+            response = await fetch('/get_bonos',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: body
+                });
+            break;
+
+        case "app.saldo":
+            response = await fetch('/get_saldo',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: body
+                });
+            break;
+
+        default:
+            response = await fetch('/chatbot',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: body
+                });
+            break;
+    }
     const data = await response.json();
     inputChat.value = '';
     console.log(data);
+    intent = data.intent;
     const botResponse = data.answer ? data.answer : "No he entendido lo que me quieres decir.";
     chatArr.push({ user: 'bot', message: botResponse });
     chatRef.innerHTML = '';
